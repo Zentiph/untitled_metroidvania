c37b7b6dfe6217b8a20b2385dcfa96217a7cda8b
Gavin <gavin.bor0925@gmail.com>
2024-05-05 15:03:01 -0400

    update gravity const

--- src/Internal/constants.py
+++ src/Internal/constants.py
@@ -4,4 +4,4 @@ Module containing useful constants to be used throughout the project.
 """
 
 # in pixels per second squared (px/sec^2)
-GRAVITY_ACCELERATION: int | float = 500
+GRAVITY_ACCELERATION: int | float = 1000
