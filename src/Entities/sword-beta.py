169ae2acff6b2d1659d27987ecae804846b92d77
Jack <jackcamp188@gmail.com>
2024-05-08 12:40:31 -0400

    edits

---
+++ src/Entities/sword-beta.py
@@ -0,0 +1,20 @@
+import pygame, sys
+pygame.init()
+
+size = width, height = 1920, 1080
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        
+    
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
+
