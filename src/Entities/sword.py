3e255611eb19d53f394d75c10bf09b41e1c1bfb3
Jack <jackcamp188@gmail.com>
2024-05-08 12:40:31 -0400

    edits

--- src/Entities/sword.py
+++ src/Entities/sword.py
@@ -1,8 +1,19 @@
 import pygame
 from pygame.sprite import _Group
-from ..Entities import player
+from ..Entities import Player
+from ..Internal import load_png
+
 
 class sword(pygame.sprite.Sprite):
     '''sword that swings and attacks enemies'''
 
-    def __init__(self)
+    def __init__(self):
+        pygame.sprite.Sprite(self)
+        self.image, self.rect = load_png("src\Media\Images\Sprites\Player\sword.png")
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.speed= 10
+
+    def sword_swing_up(self):
+        player.xcor
+        pygame.rect()
---
+++ src/Internal/load_png.py
@@ -0,0 +1,14 @@
+import pygame, os
+def load_png(name):
+    """ Load image and return image object"""
+    fullname = os.path.join("data", name)
+    try:
+        image = pygame.image.load(fullname)
+        if image.get_alpha() is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except FileNotFoundError:
+        print(f"Cannot load image: {fullname}")
+        raise SystemExit
+    return image, image.get_rect()
