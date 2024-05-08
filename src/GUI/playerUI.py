8917a480deb95d94ce15c6e83ca6561174bd03b1
Sam <tebs2000@gmail.com>
2024-05-08 10:50:45 -0400

    moreHealthBar

--- src/GUI/playerUI.py
+++ src/GUI/playerUI.py
@@ -5,15 +5,10 @@ playerUI contains various functions used the creation and utilization of graphic
 
 from pygame import Rect, draw, display
 
-def healthBar_create(inputSurface, xPos, yPos):
-    draw.rect(inputSurface, (255,0,0), Rect(xPos, yPos, 200, 30))
-    display.flip()
-
 class healthBar:
-    def __init__(
-        draw.rect(inputSurface, (255,0,0), Rect(xPos, yPos, 200, 30))
+    def __init__(self, frame, xPos, yPos):
+        draw.rect(frame, (255,0,0), Rect(xPos, yPos, 200, 30))
         display.flip()
-    ) -> None:0
         
 
 # learn classes
