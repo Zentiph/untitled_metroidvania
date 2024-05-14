7a0d21fa72ede961c5998d78d1dd688839626193
Gavin <gavin.bor0925@gmail.com>
2024-05-14 11:59:02 -0400

    add healthbar

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from typing import List
 import pygame
 
 from screeninfo import get_monitors
-from src import Entities, Internal, Level
+from src import Entities, GUI, Internal, Level
 from src.Internal import interp
 
 
@@ -37,6 +37,8 @@ plr: Entities.Player = Entities.Player(
     10,
     (255, 0, 0)
 )
+healthbar: GUI.HealthBar = GUI.HealthBar(0, Internal.SCREEN_HEIGHT - 30, plr)
+
 collision_platforms: List[Level.Surface] = [
     Level.Surface(0, 500, 800, 50, True),
     Level.Surface(400, 400, 200, 200, True),
@@ -68,7 +70,7 @@ while True:
 
     # run any update logic for the player
     plr.interp(dt)
-    plr.update(dt, collision_platforms)
+    plr.update(dt, collision_platforms, screen)
 
     # redraw the updated items on the screen
     screen.fill((0, 0, 0))
@@ -77,5 +79,7 @@ while True:
 
     for platform in collision_platforms:
         platform.draw(screen)
+
+    healthbar.update(screen)
 
     pygame.display.flip()
