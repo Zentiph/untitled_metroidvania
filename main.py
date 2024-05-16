79d21ac8c56b840d285766efbd1acf0b0f07fe5c
Nick <nickascheel@gmail.com>
2024-05-16 11:08:12 -0400

    ballas

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ while True:
         plr.moveto(500, 200, 1, interp.ease_in_out_quart)
     if keys[pygame.K_ESCAPE]:
         pygame.quit()
-
+    print("BALLSACK")
     # run any update logic for the player
     plr.interp(dt)
     plr.update(dt, collision_platforms, screen)
